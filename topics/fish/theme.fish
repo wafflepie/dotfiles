4ca47162419d078d135466ecd8bd3081dd988bfa
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-23 11:48:16 +0200

    Remove theme_newline_cursor from theme.fish

--- topics/fish/theme.fish
+++ topics/fish/theme.fish
@@ -2,5 +2,4 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme solarized
 set -g theme_git_worktree_support no
 set -g theme_project_dir_length 1
-set -g theme_newline_cursor yes
 set -g theme_date_format "+%H:%M:%S"
