f82fd444fc69d64b5041e60a78812fb0b73dcc58
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-23 11:47:55 +0200

    Clean up symlinks.sh

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -5,19 +5,18 @@ export DOTFILES="$HOME/.dotfiles"
 cd "$DOTFILES"
 
 link_file() {
-  local src=$1 dst=$2
+  local src="$1" dst="$2"
 
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
+  if [[ -f "$dst" ]] || [[ -d "$dst" ]] || [[ -L "$dst" ]]; then
+    if [[ "$overwrite_all" == "" ]] && [[ "$backup_all" == "" ]] && [[ "$skip_all" == "" ]]; then
       local current_src="$(readlink $dst)"
 
       if [ "$current_src" == "$src" ]; then
         skip=true
       else
-
         echo "File already exists: $dst ($(basename "$src")), what do you want to do?"
         echo "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
 
@@ -26,26 +25,26 @@ link_file() {
         echo ""
 
         case "$action" in
-        o)
-          overwrite=true
-          ;;
-        O)
-          overwrite_all=true
-          ;;
-        b)
-          backup=true
-          ;;
-        B)
-          backup_all=true
-          ;;
-        s)
-          skip=true
-          ;;
-        S)
-          skip_all=true
-          ;;
-        *) ;;
-
+          o)
+            overwrite=true
+            ;;
+          O)
+            overwrite_all=true
+            ;;
+          b)
+            backup=true
+            ;;
+          B)
+            backup_all=true
+            ;;
+          s)
+            skip=true
+            ;;
+          S)
+            skip_all=true
+            ;;
+          *)
+            ;;
         esac
       fi
     fi
@@ -54,24 +53,24 @@ link_file() {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]; then
+    if [[ "$overwrite" == "true" ]]; then
       rm -rf "$dst"
       echo "removed $dst"
     fi
 
-    if [ "$backup" == "true" ]; then
+    if [[ "$backup" == "true" ]]; then
       mv "$dst" "${dst}.backup"
       echo "moved $dst to ${dst}.backup"
     fi
 
-    if [ "$skip" == "true" ]; then
+    if [[ "$skip" == "true" ]]; then
       echo "skipped $src"
     fi
   fi
 
-  if [ "$skip" != "true" ]; then
+  if [[ "$skip" != "true" ]]; then
     parent=$(dirname "$2")
-    if [ ! -d "$parent" ]; then
+    if [[ ! -d "$parent" ]]; then
       mkdir -p "$parent"
     fi
 
@@ -82,7 +81,7 @@ link_file() {
 }
 
 link_all_files() {
-  local overwrite_all=false backup_all=false skip_all=false
+  local overwrite_all= backup_all= skip_all=
 
   files=$(
     find "$DOTFILES/topics" -name '.*' |
@@ -95,8 +94,6 @@ link_all_files() {
     dst="$HOME/$dst"
     link_file "$src" "$dst"
   done
-
-  link_file "$DOTFILES/.gitignore" "$HOME/.gitignore"
 }
 
 
@@ -104,7 +101,10 @@ echo "symlinking functions"
 rm -rf "$HOME/.config/fish/functions"
 ln -s "$DOTFILES/functions" "$HOME/.config/fish/functions"
 
-echo "symlinking dotfiles in topics and root .gitignore"
+echo "symlinking dotfiles in topics"
 link_all_files
+
+echo "symlinking root .gitignore"
+link_file "$DOTFILES/.gitignore" "$HOME/.gitignore"
 
 echo "symlinking done"
