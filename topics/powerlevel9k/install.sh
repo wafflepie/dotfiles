5eed1e37db47ea5c8ebe7049581ed4385c5a1f8f
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-28 09:45:36 +0200

    Add -f flag to powerlevel9k install.sh symlink

--- topics/powerlevel9k/install.sh
+++ topics/powerlevel9k/install.sh
@@ -1,1 +1,1 @@
-sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
+sudo ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
