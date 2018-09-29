83917292ae05a55e31e362f5fd19bca8550d2990
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-29 10:05:23 +0200

    Git credential improvements

---
+++ topics/git/credentials.sh
@@ -0,0 +1,20 @@
+gitconfig_local_path="$HOME/.gitconfig.local"
+
+if ! [[ -f "$gitconfig_local_path" ]]; then
+  credential_helper='cache'
+
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    credential_helper='osxkeychain'
+  fi
+
+  echo 'enter your full name'
+  read -e user_name
+  echo 'enter your e-mail'
+  read -e user_email
+
+  sed \
+    -e "s/USER_NAME/$user_name/g" \
+    -e "s/USER_EMAIL/$user_email/g" \
+    -e "s/CREDENTIAL_HELPER/$credential_helper/g" \
+    "$DOTFILES/topics/git/template.txt" >> "$gitconfig_local_path"
+fi
