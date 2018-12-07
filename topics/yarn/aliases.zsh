454cfd9b49407122592686a927366463322955f1
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-07 19:10:00 +0100

    More yarn aliases

--- topics/yarn/aliases.zsh
+++ topics/yarn/aliases.zsh
@@ -1,12 +1,14 @@
 alias y='yarn'
 alias yt='y test'
 alias yyt='y && yt'
+alias yb='y build'
+alias yyb='y && yb'
 alias yl='y lint'
 alias yyl='y && yl'
 alias ys='y start'
 alias yys='y && ys'
-alias ya="yarn add"
-alias yad="yarn add --dev"
-alias yap="yarn add --peer"
+alias ya="y add"
+alias yad="ya --dev"
+alias yap="ya --peer"
 alias yui='y upgrade-interactive --latest'
 alias lm='(ls -l node_modules; ls -l node_modules/@*) | grep ^l'
