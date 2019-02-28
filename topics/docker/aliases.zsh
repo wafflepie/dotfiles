b76a4dffc624ce01d36c226bc63fc82db3cf84bf
Václav Jančařík <vaclav.janc@gmail.com>
2019-02-28 13:38:55 +0100

    Add dcr alias

--- topics/docker/aliases.zsh
+++ topics/docker/aliases.zsh
@@ -1,6 +1,7 @@
 alias dps="docker ps"
 alias dcu="docker-compose up"
 alias dcd="docker-compose down"
+alias dcr="docker-compose restart"
 
 dbash() {
   docker exec -it $(docker ps -aqf "name=$1") bash;
