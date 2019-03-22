0d70bd7561ab984e867d28365c5f540512e9ff67
Václav Jančařík <vaclav.janc@gmail.com>
2019-03-22 11:13:53 +0100

    Add remove_containers and armageddon

--- topics/docker/aliases.zsh
+++ topics/docker/aliases.zsh
@@ -6,3 +6,16 @@ alias dcr="docker-compose restart"
 dbash() {
   docker exec -it $(docker ps -aqf "name=$1") bash;
 }
+
+remove_containers() {
+  docker stop $(docker ps -aq)
+  docker rm $(docker ps -aq)
+}
+
+armageddon() {
+  remove_containers
+  docker network prune -f
+  docker rmi -f $(docker images --filter dangling=true -qa)
+  docker volume rm $(docker volume ls --filter dangling=true -q)
+  docker rmi -f $(docker images -qa)
+}
