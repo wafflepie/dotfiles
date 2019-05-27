647eec0927e21879f8df0f0ab2e367be9bccd054
Václav Jančařík <vaclav.janc@gmail.com>
2019-05-27 13:45:43 +0200

    change screenshots location

--- topics/macos/screen_capture.sh
+++ topics/macos/screen_capture.sh
@@ -1,5 +1,7 @@
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+directory="${HOME}/Screenshots"
+mkdir -p "$directory"
+defaults write com.apple.screencapture location -string "$directory"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
