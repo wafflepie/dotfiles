1829cae4aeaeb90acfc1a08f396fbf0af43c03e6
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-02 16:32:51 +0200

    remove hyper

--- topics/hyper/hyper.sh
+++
@@ -1,4 +0,0 @@
-# NOTE: Removes gray border around the window.
-# https://github.com/zeit/hyper/issues/3259
-# https://github.com/electron/electron/issues/13164
-defaults write -app Hyper NSRequiresAquaSystemAppearance -bool No
