3addf5736b91a0dcba5b44d4b099e459eb5b3a13
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-02 16:33:09 +0200

    remove brew

--- topics/brew/install.pre.sh
+++
@@ -1,9 +0,0 @@
-if ! [[ $(which brew) ]]; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew update
-brew bundle -v
-brew upgrade
-brew cask upgrade
-brew cleanup
