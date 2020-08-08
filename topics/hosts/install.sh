8880b7d7634c9b7c20718021be8d110da47c1607
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-08 22:21:28 +0200

    fix hosts install script on fedora

--- topics/hosts/install.sh
+++ topics/hosts/install.sh
@@ -1,3 +1,4 @@
+sudo dnf install moreutils
 sudo cp /etc/hosts /etc/hosts.backup
 sudo wget https://someonewhocares.org/hosts/hosts -O /etc/hosts
 sudo bash -c "cat hosts /etc/hosts | sponge /etc/hosts"
