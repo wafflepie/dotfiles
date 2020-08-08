a530b78c3a7f1d3e9794b53ff93bb4a42eceade5
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-08 21:37:54 +0200

    update antibody install

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,3 +1,5 @@
+curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
 # NOTE: If a change in `plugins.txt` has no effect, run `antibody purge <bundle-name-here>`.
 antibody bundle < plugins.txt > ~/.zsh_plugins.sh
 antibody update
