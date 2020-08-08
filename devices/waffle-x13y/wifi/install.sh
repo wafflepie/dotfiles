72a9f0b7fe7835e5ac15fa01ea1720153f2fa0f6
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-08 21:51:38 +0200

    add waffle-x13y device

---
+++ devices/waffle-x13y/wifi/install.sh
@@ -0,0 +1,6 @@
+sudo dnf --disablerepo=\* install iw-5.4-2.fc32.x86_64.rpm 
+sudo dnf --disablerepo=\* install wpa_supplicant-2.9-3.fc32.x86_64.rpm 
+sudo dnf --disablerepo=\* install crda-3.18_2018.05.31-6.fc30.x86_64.rpm 
+sudo dnf --disablerepo=\* install wireless-tools-29-25.fc32.x86_64.rpm 
+sudo dnf --disablerepo=\* install iwl7260-firmware-25.30.13.0-110.fc32.noarch.rpm 
+sudo dnf --disablerepo=\* install NetworkManager-wifi-1.22.14-1.fc32.x86_64.rpm 
