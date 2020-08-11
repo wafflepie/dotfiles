c562dd68ad3cc4f6a0873014a9f5cfb5875ad727
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-11 21:04:32 +0200

    add missing user theme gnome extension

--- topics/gnome/theme.post.sh
+++ topics/gnome/theme.post.sh
@@ -3,5 +3,6 @@ sudo dnf install \
   gnome-shell-extension-dash-to-dock \
   gnome-shell-extension-drive-menu \
   gnome-shell-extension-gpaste \
+  gnome-shell-extension-user-theme \
   gnome-tweaks \
   papirus-icon-theme \
