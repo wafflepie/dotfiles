fe3b5e90876a808000cfdae64261236baa47ac8f
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 16:48:39 +0200

    assume standard group for x13y wifi

--- devices/waffle-x13y/wifi/install.sh
+++
@@ -1,6 +0,0 @@
-sudo dnf --disablerepo=\* install ./iw-5.4-2.fc32.x86_64.rpm \
-  ./wpa_supplicant-2.9-3.fc32.x86_64.rpm \
-  ./crda-3.18_2018.05.31-6.fc30.x86_64.rpm \
-  ./wireless-tools-29-25.fc32.x86_64.rpm \
-  ./iwl7260-firmware-25.30.13.0-110.fc32.noarch.rpm \
-  ./NetworkManager-wifi-1.22.14-1.fc32.x86_64.rpm \
