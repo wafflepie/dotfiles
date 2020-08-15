df9763d6ca7af3932fbc7d047f545d97e28c9bb4
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 10:59:57 +0200

    add communication topic

---
+++ topics/communication/slack.sh
@@ -0,0 +1,3 @@
+if ! rpm -q slack; then
+  sudo dnf install https://downloads.slack-edge.com/linux_releases/slack-4.8.0-0.1.fc21.x86_64.rpm
+fi
---
+++ topics/communication/teams.sh
@@ -0,0 +1,3 @@
+if ! rpm -q teams; then
+  sudo dnf install https://packages.microsoft.com/yumrepos/ms-teams/teams-1.3.00.16851-1.x86_64.rpm
+fi
