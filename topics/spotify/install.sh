58ae3d311baa122c487863b4e027b2d9843b027b
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 10:27:49 +0200

    use spotify install script in gui

--- topics/spotify/install.sh
+++ topics/spotify/install.sh
@@ -1,7 +1,5 @@
 sudo dnf install lpf lpf-spotify-client
 
-if ! lpf state spotify-client | grep -q OK; then
-  sudo lpf approve spotify-client
-  sudo lpf build spotify-client
-  sudo lpf install spotify-client
+if ! lpf state spotify-client | grep -q OK && [[ ! -z "$XDG_CURRENT_DESKTOP" ]]; then
+  sudo lpf update spotify-client
 fi
