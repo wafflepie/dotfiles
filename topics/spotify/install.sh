59f15366a4ebb965cd94dfe58e0d069a80a1f83e
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 16:56:46 +0200

    fix spotify install script without gui

--- topics/spotify/install.sh
+++ topics/spotify/install.sh
@@ -1,5 +1,5 @@
 sudo dnf install lpf lpf-spotify-client
 
-if ! lpf state spotify-client | grep -q OK && [[ ! -z "$XDG_CURRENT_DESKTOP" ]]; then
+if [[ ! -z "$XDG_CURRENT_DESKTOP" ]] && ! lpf state spotify-client | grep -q OK; then
   sudo lpf update spotify-client
 fi
