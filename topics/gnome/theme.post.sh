4ad6ac341d2782145908a7e0a438b4f38a0bfbf1
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 10:31:51 +0200

    add vimix theme install script

--- topics/gnome/theme.post.sh
+++ topics/gnome/theme.post.sh
@@ -6,3 +6,14 @@ sudo dnf install \
   gnome-shell-extension-user-theme \
   gnome-tweaks \
   papirus-icon-theme \
+
+sudo dnf install gtk-murrine-engine gtk2-engines
+
+vimix_path="$HOME/Repositories/vimix-gtk-themes"
+
+if [[ ! -d "$vimix_path" ]]; then
+  mkdir -p "$vimix_path"
+  git clone https://github.com/vinceliuice/vimix-gtk-themes.git "$vimix_path"
+  cd "$vimix_path"
+  ./install.sh
+fi
