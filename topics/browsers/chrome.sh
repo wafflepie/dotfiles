9aedc74b268d80884dccd0fb9447a57307433fba
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-15 10:59:50 +0200

    update chrome install script

--- topics/browsers/chrome.sh
+++ topics/browsers/chrome.sh
@@ -1,2 +1,5 @@
-sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+if ! rpm -q google-chrome-stable; then
+  sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+fi
+
 sudo dnf install chrome-gnome-shell
