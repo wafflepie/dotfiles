e50601d8a5ec2567acfa79c04d8a384b72546c65
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-18 22:08:18 +0200

    remove redundant rpmfusion repo from tlp script

--- devices/waffle-x13y/tlp.sh
+++ devices/waffle-x13y/tlp.sh
@@ -1,5 +1,4 @@
 sudo dnf install tlp tlp-rdw
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
 sudo dnf install akmod-acpi_call
 sudo tlp start
