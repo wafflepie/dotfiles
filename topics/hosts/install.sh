8e8fd3e93ac17271fb3a52bcb8607484a3e19e6b
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-23 17:43:51 +0200

    add moreutils topic

--- topics/hosts/install.sh
+++ topics/hosts/install.sh
@@ -1,4 +1,3 @@
-sudo dnf install moreutils
 sudo cp /etc/hosts /etc/hosts.backup
 sudo wget https://someonewhocares.org/hosts/hosts -O /etc/hosts
 sudo bash -c "cat hosts /etc/hosts | sponge /etc/hosts"
