234f787f3bbebeedca2a781b6230fb55569e053e
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-23 11:56:52 +0200

    improve pia install script

--- topics/pia/install.sh
+++ topics/pia/install.sh
@@ -1,3 +1,5 @@
-wget https://www.privateinternetaccess.com/installer/pia-nm.sh
-sudo bash ./pia-nm.sh
-rm -f pia-nm.sh
+if ! ls /etc/NetworkManager/system-connections | grep -q PIA; then
+  wget https://www.privateinternetaccess.com/installer/pia-nm.sh
+  sudo bash ./pia-nm.sh
+  rm -f pia-nm.sh
+fi
