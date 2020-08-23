2c0e9b36a2097890f39c8f845e3aebc0d793d10c
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-23 12:19:45 +0200

    upgrade p9k to p10k

---
+++ topics/p10k/config.zsh
@@ -0,0 +1,14 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs newline)
+
+POWERLEVEL9K_STATUS_VERBOSE=true
+POWERLEVEL9K_STATUS_OK=false
