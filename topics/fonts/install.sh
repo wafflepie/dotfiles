05b5e6cec6c0823ba8855f958ce4ce7baccd131d
Václav Jančařík <vaclav.janc@gmail.com>
2020-08-30 17:18:03 +0200

    update font install script

--- topics/fonts/install.sh
+++ topics/fonts/install.sh
@@ -1,20 +1,17 @@
 fonts_directory="$HOME/.fonts"
 
 mkdir -p "$fonts_directory"
-
-urls=(
-  'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip'
-)
-
 cd "$fonts_directory"
 
-for url in "$urls"; do
-  wget -q -O font.zip "$url" && unzip -o font.zip && rm font.zip
-done
-
-find . -iname "Fura Code*" -delete
-find . -iname "*.ttf" -delete
-find . -iname "*Windows Compatible*" -delete
+if ! find . -name 'Fira*' -printf 1 -quit | grep -q 1; then
+  url='https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip'
+  wget -O font.zip "$url"
+  unzip -o font.zip
+  rm font.zip
+  find . -iname "Fura*" -delete
+  find . -iname "Fira*.ttf" -delete
+  find . -iname "Fira*Windows Compatible*" -delete
+fi
 
 sudo dnf install google-roboto-fonts
 
