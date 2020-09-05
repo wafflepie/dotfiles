f3ae18e946c802165d81a2d4bf074e8321a556a2
Václav Jančařík <vaclav.janc@gmail.com>
2020-09-05 16:17:29 +0200

    fix code symlinking script

--- topics/code/symlinks.post.sh
+++ topics/code/symlinks.post.sh
@@ -1,4 +1,4 @@
-if command -v code; then
+if command -v code /dev/null; then
   if is_wsl; then
     code_remote_directory="$HOME/.vscode-server"
     code_local_directory="$HOME/winhome/AppData/Roaming/Code"
