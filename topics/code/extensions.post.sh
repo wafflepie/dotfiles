681771f5602a6e92f6e5b2be43abdc9047759c08
Václav Jančařík <vaclav.janc@gmail.com>
2020-09-05 16:18:00 +0200

    implement code extension handling for WSL

--- topics/code/extensions.post.sh
+++ topics/code/extensions.post.sh
@@ -1,5 +1,14 @@
-if ! is_wsl; then
-  for extension in $(cat extensions.txt); do
-    code --install-extension "$extension"
-  done
+extensions=$(cat extensions.txt)
+
+# NOTE: This is purely to suppress warnings that UNC paths are not supported.
+if is_wsl; then
+  cd "$HOME/winhome"
 fi
+
+for extension in $extensions; do
+  if is_wsl; then
+    cmd.exe /C code --install-extension "$extension"
+  else
+    code --install-extension "$extension"
+  fi
+done
