8370ad33f64dff0eb6f1de6f8a992551bfe414e5
Václav Jančařík <vaclav.janc@gmail.com>
2020-09-06 19:47:49 +0200

    minor improvement in wsl install script

--- topics/wsl/install.pre.sh
+++ topics/wsl/install.pre.sh
@@ -1,5 +1,5 @@
 if is_wsl; then
-  if is_opensuse && ! command -v wslvar; then
+  if is_opensuse && ! command -v wslvar > /dev/null; then
     sudo zypper addrepo https://download.opensuse.org/repositories/home:/wslutilities/openSUSE_Leap_15.1/home:wslutilities.repo
     sudo zypper refresh
     sudo zypper install wslu
