db104ce6745e5934f9355879ff89070ed58b5550
Václav Jančařík <vaclav.janc@gmail.com>
2020-12-05 20:51:32 +0100

    add brew topic

---
+++ topics/brew/install.pre.sh
@@ -0,0 +1,10 @@
+if is_mac; then
+  if ! [[ $(which brew) ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  brew update
+  brew bundle -v
+  brew upgrade
+  brew cleanup
+fi
