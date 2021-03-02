3ba198bcb0d1c8005145651aa612c23215bdb5e5
Václav Jančařík <vaclav.janc@gmail.com>
2021-03-02 23:00:21 +0100

    add chrome topic

---
+++ topics/chrome/install.sh
@@ -0,0 +1,7 @@
+if ! is_wsl && is_fedora; then
+  if ! rpm -q google-chrome-stable; then
+    sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+  fi
+
+  sudo dnf install chrome-gnome-shell
+fi
