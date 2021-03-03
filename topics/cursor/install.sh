6efc9b3ea1afd2a2fb90e3d67bb6c14a5908e332
Václav Jančařík <vaclav.janc@gmail.com>
2021-03-03 20:33:38 +0100

    fix prompt in cursor install script

--- topics/cursor/install.sh
+++ topics/cursor/install.sh
@@ -1,4 +1,4 @@
-if ! is_wsl && is_fedora; then
+if ! is_wsl && is_fedora && ! rpm -q bibata-cursor-themes; then
   sudo dnf install inkscape xcursorgen
   sudo dnf copr enable peterwu/rendezvous
   sudo dnf install bibata-cursor-themes
