28a8ea039f806c1455661f71c58d962bfa593113
Václav Jančařík <vaclav.janc@gmail.com>
2021-03-03 20:33:51 +0100

    make fedora install script faster

--- topics/fedora/install.pre.sh
+++ topics/fedora/install.pre.sh
@@ -1,15 +1,17 @@
 if is_fedora; then
-  sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  sudo dnf install \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+    rpmfusion-free-release-tainted \
+    rpmfusion-nonfree-release-tainted \
+    fedora-workstation-repositories \
 
-  sudo dnf install rpmfusion-free-release-tainted
-  sudo dnf install rpmfusion-nonfree-release-tainted
-
-  sudo dnf install fedora-workstation-repositories
-
-  if ! is_wsl; then
+  if is_wsl; then
+    sudo dnf install -y @"C Development Tools and Libraries"
+  else
     sudo dnf install -y \
       @base-x \
+      @"C Development Tools and Libraries" \
       @"Common NetworkManager Submodules" \
       @Core \
       @Fonts \
@@ -19,6 +21,4 @@ if is_fedora; then
       @"Printing Support" \
       @Standard
   fi
-
-  sudo dnf install -y @"C Development Tools and Libraries"
 fi
