33f32b17581c40223fd6b1be728ebdc5e1168d68
Václav Jančařík <vaclav.janc@gmail.com>
2021-07-03 15:53:35 +0200

    improve android emulation scripts

--- topics/android/config.zsh
+++ topics/android/config.zsh
@@ -1,2 +1,9 @@
 export PATH="$HOME/Android/Sdk/platform-tools:$PATH"
 export ANDROID_SDK="$HOME/Android/Sdk"
+
+winavd() {
+  # NOTE: This is purely to suppress warnings that UNC paths are not supported.
+  cd /mnt/c
+  cmd.exe /C "emulator -avd $@"
+  cd - > /dev/null
+}
--- topics/expo/scripts.zsh
+++ topics/expo/scripts.zsh
@@ -1,15 +1,9 @@
-expo_proxy() {
+winexpo() {
   EXPO_TOPIC_PATH=$(wslpath -w "$DOTFILES/topics/expo")
 
   # NOTE: This is purely to suppress warnings that UNC paths are not supported.
   cd /mnt/c
-  cmd.exe /C "pushd $EXPO_TOPIC_PATH && powershell.exe -File proxy.ps1"
-  cd - > /dev/null
-}
-
-expo_hostname() {
-  # NOTE: This is purely to suppress warnings that UNC paths are not supported.
-  cd /mnt/c
+  cmd.exe /C "pushd $EXPO_TOPIC_PATH && powershell.exe -File portproxy.ps1"
   export REACT_NATIVE_PACKAGER_HOSTNAME=$(powershell.exe -Command "(Get-NetIPAddress -InterfaceAlias Wi-Fi -AddressFamily IPv4).IPAddress")
   cd - > /dev/null
 }
