1bf88bf231e0ec162f74e0346edfc889c3b40e10
Václav Jančařík <vaclav.janc@gmail.com>
2021-07-03 16:55:23 +0200

    fix thefuck install script

--- topics/thefuck/install.post.sh
+++ topics/thefuck/install.post.sh
@@ -1,1 +1,7 @@
-pip3 install thefuck
+# https://github.com/pypa/pip/issues/7883#issuecomment-852102601
+if is_wsl; then
+  unset DISPLAY
+fi
+
+pip3 install thefuck --user
+run_topic_script topics/x/display.zsh
