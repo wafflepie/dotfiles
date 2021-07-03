216f4b38e7324dc76c8c8d0cbf5788846c2b5a47
Václav Jančařík <vaclav.janc@gmail.com>
2021-07-03 15:17:10 +0200

    always export android vars

--- topics/android/config.zsh
+++ topics/android/config.zsh
@@ -1,4 +1,2 @@
-if ! is_wsl && is_fedora; then
-  export PATH="$HOME/Android/Sdk/platform-tools:$PATH"
-  export ANDROID_SDK="$HOME/Android/Sdk"
-fi
+export PATH="$HOME/Android/Sdk/platform-tools:$PATH"
+export ANDROID_SDK="$HOME/Android/Sdk"
