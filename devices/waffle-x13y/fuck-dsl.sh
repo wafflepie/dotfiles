6b8ab23227327c741936145803c0f8d896730152
Václav Jančařík <vaclav.janc@gmail.com>
2021-08-02 18:09:33 +0200

    remove hacky modification of xmm7360.c

--- devices/waffle-x13y/fuck-dsl.sh
+++ devices/waffle-x13y/fuck-dsl.sh
@@ -6,9 +6,6 @@ cd "$SCRIPT_DIR/../../submodules/xmm7360-pci"
 git clean -dfx
 git restore .
 
-# https://github.com/xmm7360/xmm7360-pci/issues/96
-sed -i '1332d' xmm7360.c
-
 if [[ ! -f xmm7360.ini ]]; then
   cp xmm7360.ini.sample xmm7360.ini
   sed -i 's/your.apn.here/internet.t-mobile.cz/' xmm7360.ini
