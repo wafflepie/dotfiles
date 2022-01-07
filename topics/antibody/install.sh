2ecff0db02c640a9f62effa9f6c3cea62d0c8812
Václav Jančařík <vaclav.janc@gmail.com>
2022-01-07 15:54:47 +0100

    use submodules instead of antibody

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,5 +1,1 @@
-curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
-
-# NOTE: If a change in `plugins.txt` has no effect, run `antibody purge <bundle-name-here>`.
-antibody bundle < plugins.txt > ~/.zsh_plugins.sh
-antibody update
+cat plugins.txt > ~/.zsh_plugins.sh
